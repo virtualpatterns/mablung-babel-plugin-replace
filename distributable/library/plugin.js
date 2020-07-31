71427f63338049d2b5cb1dd6885b77b050b196d3
virtualpatterns <code.virtualpatterns@gmail.com>
2020-07-30 22:30:02 -0400

    post-test

--- distributable/library/plugin.js
+++ distributable/library/plugin.js
@@ -7,6 +7,10 @@ exports.Plugin = void 0;
 
 var ChangeCase = _interopRequireWildcard(require("change-case"));
 
+var _util = _interopRequireDefault(require("util"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
@@ -18,7 +22,7 @@ const {
 class Plugin {
   static createPlugin(visitorClass) {
     // console.log(`Plugin.createPlugin(${visitorClass.name})`)
-    return function (babel) {
+    return _util.default.deprecate(function (babel) {
       // console.log(`Plugin.createPlugin(${visitorClass.name})(babel)`)
       let visitorInstance = new visitorClass(babel);
       let pluginObject = {};
@@ -40,7 +44,7 @@ class Plugin {
       });
       pluginObject.visitor = visitorObject;
       return pluginObject;
-    };
+    }, 'The package \'@virtualpatterns/mablung-babel-plugin-replace\' has been deprecated.  Use the \'@virtualpatterns/mablung-babel-plugin-replace-identifier\' package instead ... it offers the same functionality and options.');
   }
 
 }
